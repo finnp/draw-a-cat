60cac8b695ca52520b207720085ffa4dcf287c0c
Finn <derfinn@gmail.com>
2015-05-10 22:46:32 +0200

    fix browserify build

--- compile.js
+++ compile.js
@@ -10,13 +10,14 @@ var entries = [{from: __dirname + '/app.js', to: __dirname + '/bundle.js'}]
 menu.forEach(function (item) {
   var id = util.idFromName(item)
   entries.push({
-    from: __dirname + '/exercises/' + id + '/index.js -t brfs',
-    to: __dirname + '/exercises/' + id + '/bundle.js -t brfs'
+    from: __dirname + '/exercises/' + id + '/index.js',
+    to: __dirname + '/exercises/' + id + '/bundle.js'
   })
 })
 
 entries.forEach(function (entry) {
   var b = browserify()
+  b.transform('brfs')
   b.add(entry.from)
   b.bundle().pipe(fs.createWriteStream(entry.to))
 })
